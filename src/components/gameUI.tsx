bdd427d8d595b6535488a606bd1508577c895aff
Rahul Biswakarma <rahul.id39@gmail.com>
2023-02-16 14:28:29 +0530

    created game ui component

---
+++ src/components/gameUI.tsx
@@ -0,0 +1,21 @@
+export default function UI() {
+	return (
+		<section className="flex flex-col bg-black gap-[2px] w-max">
+			<div className="flex gap-[2px]">
+				<span className="bg-red-500 w-[100px] h-[100px]"></span>
+				<span className="bg-red-500 w-[100px] h-[100px]"></span>
+				<span className="bg-red-500 w-[100px] h-[100px]"></span>
+			</div>
+			<div className="flex gap-[2px]">
+				<span className="bg-red-500 w-[100px] h-[100px]"></span>
+				<span className="bg-red-500 w-[100px] h-[100px]"></span>
+				<span className="bg-red-500 w-[5100px]"></span>
+			</div>
+			<div className="flex gap-[2px]">
+				<span className="bg-red-500 w-[100px] h-[100px]"></span>
+				<span className="bg-red-500 w-[100px] h-[100px]"></span>
+				<span className="bg-red-500 w-[100px] h-[100px]"></span>
+			</div>
+		</section>
+	);
+}
